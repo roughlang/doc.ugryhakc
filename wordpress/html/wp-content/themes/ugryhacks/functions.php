003aec7e6e5b6e4908bfcaa7ca031d70926c9659
osamthing <osamthing@gmail.com>
2021-10-21 10:27:33 +0900

    modal部分（手動対応）

--- wordpress/html/wp-content/themes/ugryhacks/functions.php
+++ wordpress/html/wp-content/themes/ugryhacks/functions.php
@@ -18,3 +18,43 @@ function my_theme_setup() {
   add_theme_support('post-thumbnails');
 }
 add_action( 'after_setup_theme', 'my_theme_setup');
+
+/* 記事編集時のデフォルトの文字列 */
+add_filter('the_editor_content', lalala_default_content);
+function lalala_default_content($content) {
+    if (empty($content)) {
+      $default = <<<EOM
+
+
+------------------------
+- [https://localhost/](https://localhost/)
+
+
+<!-- Button trigger modal -->
+<!--
+<a data-bs-toggle="modal" data-bs-target="#modalid_{$id}">
+  <img src="{$url}" width="100%" alt="{$title}">
+</a>
+-->
+<!-- Modal -->
+<!--
+<div class="modal fade" id="modalid_{$id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-xl">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <img src="{$url}" width="100%" alt="{$title}">
+      </div>
+
+    </div>
+  </div>
+</div>
+-->
+EOM;
+        return $default;
+    } else {
+        return $content;
+    }
+}
